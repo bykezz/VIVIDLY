e33281b390e49f870061175d961ed37ac1bd2711
bykezz <chibuikepascal31@gmail.com>
2024-07-20 14:41:22 +0100

    Adding Customers Route

--- routes/customers.js
+++ routes/customers.js
@@ -45,6 +45,45 @@ router.post("/", async (req, res) => {
   res.send(customer);
 });
 
+router.put("/:id", async (req, res) => {
+  const { error } = validateCustomer(req.body);
+  if (error) {
+    return res.status(400).send(error.details[0].message);
+  }
+
+  const customer = await Customer.findByIdAndUpdate(
+    req.params.id,
+    {
+      name: req.body.name,
+      phone: req.body.phone,
+      isGold: req.body.isGold,
+    },
+    { new: true }
+  );
+
+  if (!customer) {
+    return res.status(404).send("The genre with the given ID was not found.");
+  }
+  res.send(customer);
+});
+
+router.get("/:id", async (req, res) => {
+  const customer = await Customer.findById(req.params.id);
+
+  if (!customer) {
+    return res.status(404).send("The genre with the given ID was not found.");
+  }
+  res.send(customer);
+});
+
+router.delete("/:id", async (req, res) => {
+  const customer = await Customer.findByIdAndDelete(req.params.id);
+  if (!customer) {
+    return res.status(404).send("The genre with the given ID was not found.");
+  }
+  res.send(customer);
+});
+
 const validateCustomer = (customer) => {
   const schema = {
     name: Joi.string().min(5).max(50).required(),
