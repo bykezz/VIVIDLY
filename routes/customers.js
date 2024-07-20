d3fb64e560263c88b82fb900ed4be1ff324533d5
bykezz <chibuikepascal31@gmail.com>
2024-07-20 14:35:43 +0100

    Adding Customers Route

--- routes/customers.js
+++ routes/customers.js
@@ -36,7 +36,11 @@ router.post("/", async (req, res) => {
     return res.status(400).send(error.details[0].message);
   }
 
-  let customer = new Customer({ name: req.body.name });
+  let customer = new Customer({
+    name: req.body.name,
+    phone: req.body.phone,
+    isGold: req.body.isGold,
+  });
   customer = await customer.save();
   res.send(customer);
 });
