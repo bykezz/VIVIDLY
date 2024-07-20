bf5cdbf50148cfebabbab0a841b695ccbc525278
bykezz <chibuikepascal31@gmail.com>
2024-07-20 13:52:09 +0100

    using mongoDB and mongoose

--- routes/genres.js
+++ routes/genres.js
@@ -45,21 +45,20 @@ router.put("/:id", async (req, res) => {
   res.send(genre);
 });
 
-router.get("/:id", (req, res) => {
-  const genre = genres.find((c) => c.id === parseInt(req.params.id));
+router.get("/:id", async (req, res) => {
+  const genre = await Genre.findById(req.params.id);
+
   if (!genre) {
     return res.status(404).send("The genre with the given ID was not found.");
   }
   res.send(genre);
 });
 
-router.delete("/:id", (req, res) => {
-  const genre = genres.find((c) => c.id === parseInt(req.params.id));
+router.delete("/:id", async (req, res) => {
+  const genre = await Genre.findByIdAndRemove(req.params.id);
   if (!genre) {
     return res.status(404).send("The genre with the given ID was not found.");
   }
-  const index = genres.indexOf(genre);
-  genres.splice(index, 1);
   res.send(genre);
 });
 
