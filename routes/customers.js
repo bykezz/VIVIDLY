6f6932f4e884557465372af0cbd2b5224f9e7198
bykezz <chibuikepascal31@gmail.com>
2024-07-20 14:57:41 +0100

    Adding Customers Route

--- routes/customers.js
+++ routes/customers.js
@@ -8,7 +8,7 @@ router.get("/", async (req, res) => {
 });
 
 router.post("/", async (req, res) => {
-  const { error } = validateCustomer(req.body);
+  const { error } = validate(req.body);
   if (error) {
     return res.status(400).send(error.details[0].message);
   }
@@ -23,7 +23,7 @@ router.post("/", async (req, res) => {
 });
 
 router.put("/:id", async (req, res) => {
-  const { error } = validateCustomer(req.body);
+  const { error } = validate(req.body);
   if (error) {
     return res.status(400).send(error.details[0].message);
   }
