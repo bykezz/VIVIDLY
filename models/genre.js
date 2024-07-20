29576862359bed36fd69ab3ef32bd268f6d3c9f2
bykezz <chibuikepascal31@gmail.com>
2024-07-20 14:58:07 +0100

    Adding Customers Route

---
+++ models/genre.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+const Joi = require("joi");
+
+const Genre = mongoose.model(
+  "Genre",
+  new mongoose.Schema({
+    name: {
+      type: String,
+      required: true,
+      minlength: 5,
+      maxlength: 50,
+    },
+  })
+);
+
+const validateGenre = (genre) => {
+  const schema = {
+    name: Joi.string().min(3).required(),
+  };
+  return Joi.validate(genre, schema);
+};
+
+exports.Customer = Customer;
+exports.validate = validateCustomer;
--- routes/genres.js
+++ routes/genres.js
@@ -1,19 +1,5 @@
-const mongoose = require("mongoose");
 const express = require("express");
-const Joi = require("joi");
 const router = express.Router();
-
-const Genre = mongoose.model(
-  "Genre",
-  new mongoose.Schema({
-    name: {
-      type: String,
-      required: true,
-      minlength: 5,
-      maxlength: 50,
-    },
-  })
-);
 
 router.get("/", async (req, res) => {
   const genres = await Genre.find().sort("name");
@@ -67,12 +53,5 @@ router.delete("/:id", async (req, res) => {
   }
   res.send(genre);
 });
-
-const validateGenre = (genre) => {
-  const schema = {
-    name: Joi.string().min(3).required(),
-  };
-  return Joi.validate(genre, schema);
-};
 
 module.exports = router;
