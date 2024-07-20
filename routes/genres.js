400afddc861b7e92fec4431376ce579f673a0965
bykezz <chibuikepascal31@gmail.com>
2024-07-20 13:40:42 +0100

    using mongoDB and mongoose

--- routes/genres.js
+++ routes/genres.js
@@ -19,14 +19,14 @@ router.get("/", async (req, res) => {
   const genres = await Genre.find().sort("name");
   res.send(genres);
 });
-router.post("/", (req, res) => {
+router.post("/", async (req, res) => {
   const { error } = validateGenre(req.body);
   if (error) {
     return res.status(400).send(error.details[0].message);
   }
 
-  const genre = new Genre({ name: req.body.name });
-  genres.push(genre);
+  let genre = new Genre({ name: req.body.name });
+  genre = await genre.save();
   res.send(genre);
 });
 router.put("/:id", (req, res) => {
