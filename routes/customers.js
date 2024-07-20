9894c057ff4d695a018fe75559d83232dacaea17
bykezz <chibuikepascal31@gmail.com>
2024-07-20 14:46:15 +0100

    Adding Customers Route

--- routes/customers.js
+++ routes/customers.js
@@ -1,29 +1,6 @@
-const mongoose = require("mongoose");
 const express = require("express");
 const Joi = require("joi");
 const router = express.Router();
-
-const Customer = mongoose.model(
-  "Customer",
-  new mongoose.Schema({
-    name: {
-      type: String,
-      required: true,
-      minlength: 5,
-      maxlength: 50,
-    },
-    isGold: {
-      type: Boolean,
-      default: false,
-    },
-    phone: {
-      type: String,
-      required: true,
-      minlength: 5,
-      maxlength: 50,
-    },
-  })
-);
 
 router.get("/", async (req, res) => {
   const customers = await Customer.find().sort("name");
