ff419bde71cb60fa67fae1f937a6feccd30cf350
bykezz <chibuikepascal31@gmail.com>
2024-07-20 13:30:22 +0100

    using mongoDB and mongoose

--- routes/genres.js
+++ routes/genres.js
@@ -3,14 +3,17 @@ const express = require("express");
 const Joi = require("joi");
 const router = express.Router();
 
-const genreSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    minlength: 5,
-    maxlength: 50,
-  },
-});
+const Genre = new mongoose.model(
+  "Genre",
+  new mongoose.Schema({
+    name: {
+      type: String,
+      required: true,
+      minlength: 5,
+      maxlength: 50,
+    },
+  })
+);
 
 const genres = [
   { id: 1, name: "Action" },
