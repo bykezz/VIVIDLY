e94eca6e74b52cf5ff512217f671cb8a7d421892
bykezz <chibuikepascal31@gmail.com>
2024-07-20 13:53:31 +0100

    using mongoDB and mongoose

--- routes/genres.js
+++ routes/genres.js
@@ -3,7 +3,7 @@ const express = require("express");
 const Joi = require("joi");
 const router = express.Router();
 
-const Genre = new mongoose.model(
+const Genre = mongoose.model(
   "Genre",
   new mongoose.Schema({
     name: {
