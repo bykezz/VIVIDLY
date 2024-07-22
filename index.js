847e32add99aad16d4a136f024f419d7942a2586
bykezz <chibuikepascal31@gmail.com>
2024-07-22 16:31:20 +0100

    Implementing Fawn

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const mongoose = require("mongoose");
 
 const genres = require("./routes/genres");
 const customers = require("./routes/customers");
+const movies = require("./routes/movies");
+const rentals = require("./routes/rentals");
 
 const app = express();
 mongoose
@@ -14,6 +16,8 @@ app.use(express.json());
 
 app.use("/api/genres", genres);
 app.use("/api/customers", customers);
+app.use("/api/movies", movies);
+app.use("/api/rentals", rentals);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}....`));
