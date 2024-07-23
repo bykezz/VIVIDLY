6e5690afde396ce972d11bbd8544b9cb63fb7dc4
bykezz <chibuikepascal31@gmail.com>
2024-07-23 16:02:16 +0100

    Implementing Auth

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bcrypt = require("bcrypt");
 const _ = require("lodash");
-const { User, validate } = require("../models/user");
+const { User } = require("../models/user");
 const mongoose = require("mongoose");
 const router = express.Router();
 
@@ -11,7 +11,7 @@ router.post("/", async (req, res) => {
     return res.status(400).send(error.details[0].message);
   }
   let user = await User.findOne({ email: req.body.email });
-  if (user) return res.status(400).send("User already registered.");
+  if (!user) return res.status(400).send("Invalid email or password.");
 
   user = new User(_.pick(req.body, ["_id", "name", "email", "password"]));
   const salt = await bcrypt.genSalt(10);
@@ -20,5 +20,13 @@ router.post("/", async (req, res) => {
 
   res.send(_.pick(user, ["_id", "name", "email"]));
 });
+
+const validateUser = (req) => {
+  const schema = {
+    email: Joi.string().min(5).max(255).required().email(),
+    password: Joi.string().min(5).max(1024).required(),
+  };
+  return Joi.validate(req, schema);
+};
 
 module.exports = router;
