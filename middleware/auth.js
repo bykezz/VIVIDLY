63db37582dce34aa7dcf4c645afb1939f63d6b98
bykezz <chibuikepascal31@gmail.com>
2024-07-23 17:29:28 +0100

    Implementing jwt

--- middleware/auth.js
+++ middleware/auth.js
@@ -4,4 +4,6 @@ const config = require("config");
 function auth(req, res, next) {
   const token = req.header("x-auth-token");
   if (!token) res.status(401).send("Access denied. No token provided.");
+
+  jwt.verify(token, config.get("jwtPrivateKey"));
 }
