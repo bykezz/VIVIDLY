c3cb6425dfcf47690f64f9d66ae7c4d7ae0a43f8
bykezz <chibuikepascal31@gmail.com>
2024-07-23 16:13:41 +0100

    Implementing Auth

--- routes/auth.js
+++ routes/auth.js
@@ -19,7 +19,7 @@ router.post("/", async (req, res) => {
   res.send(true);
 });
 
-const validateUser = (req) => {
+const validate = (req) => {
   const schema = {
     email: Joi.string().min(5).max(255).required().email(),
     password: Joi.string().min(5).max(1024).required(),
