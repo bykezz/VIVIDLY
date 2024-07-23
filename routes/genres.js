401b815f6a407dff920e9a25da56792c886728fa
bykezz <chibuikepascal31@gmail.com>
2024-07-23 18:18:57 +0100

    Implementing auth middleware

--- routes/genres.js
+++ routes/genres.js
@@ -48,7 +48,7 @@ router.get("/:id", async (req, res) => {
   res.send(genre);
 });
 
-router.delete("/:id", auth, async (req, res) => {
+router.delete("/:id", [auth, admin], async (req, res) => {
   const genre = await Genre.findByIdAndDelete(req.params.id);
   if (!genre) {
     return res.status(404).send("The genre with the given ID was not found.");
