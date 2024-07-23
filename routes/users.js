5a472b17111d147655190e518e6846757eed9866
bykezz <chibuikepascal31@gmail.com>
2024-07-23 16:56:25 +0100

    Implementing jwt

--- routes/users.js
+++ routes/users.js
@@ -18,7 +18,11 @@ router.post("/", async (req, res) => {
   user.password = await bcrypt.hash(user.password, salt);
   await user.save();
 
-  res.send(_.pick(user, ["_id", "name", "email"]));
+  const token = jwt.sign({ _id: user.id }, config.get("jwtPrivateKey"));
+  res.header(
+    "x-auth-token",
+    token.send(_.pick(user, ["_id", "name", "email"]))
+  );
 });
 
 module.exports = router;
