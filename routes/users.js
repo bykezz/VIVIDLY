eb30b0e953dfca98104e461a78ea6009d4895ecf
bykezz <chibuikepascal31@gmail.com>
2024-07-23 14:48:58 +0100

    Adding the user model

--- routes/users.js
+++ routes/users.js
@@ -4,8 +4,14 @@ const mongoose = require("mongoose");
 const router = express.Router();
 
 router.post("/", async (req, res) => {
-  const genres = await Genre.find().sort("name");
-  res.send(genres);
+  const { error } = validate(req.body);
+  if (error) {
+    return res.status(400).send(error.details[0].message);
+  }
+  User.findOne({ email: req.body.email });
+  const user = new User({ name: req.body.name });
+  await user.save();
+  res.send(user);
 });
 
 module.exports = router;
