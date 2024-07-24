5ca39a5802174df31419ca3f1e14168b3e0bb6f9
bykezz <chibuikepascal31@gmail.com>
2024-07-24 13:39:12 +0100

    Restructuring index.js

--- index.js
+++ index.js
@@ -1,17 +1,11 @@
 const express = require("express");
-require("express-async-errors");
-const config = require("config");
-const Joi = require("joi");
-Joi.objectId = require("joi-objectid")(Joi);
-
 const app = express();
+
+require("./startup/logging");
 require("./startup/routes")(app);
 require("./startup/db")();
-
-if (!config.get("jwtPrivateKey")) {
-  console.error("FATAL ERROR: jwtPrivateKey is not defined");
-  process.exit(1);
-}
+require("./startup/config")();
+require("./startup/validation");
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}....`));
--- startup/logging.js
+++ startup/logging.js
@@ -1,5 +1,6 @@
 const winston = require("winston");
 require("winston-mongodb");
+require("express-async-errors");
 
 module.exports = function () {
   winston.exceptions.handle(
