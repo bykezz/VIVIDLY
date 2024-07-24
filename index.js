9bf411d6f2a6f5d328acf4eedc25071ecd12d9a3
bykezz <chibuikepascal31@gmail.com>
2024-07-24 13:39:45 +0100

    Restructuring index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ require("./startup/logging");
 require("./startup/routes")(app);
 require("./startup/db")();
 require("./startup/config")();
-require("./startup/validation");
+require("./startup/validation")();
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}....`));
