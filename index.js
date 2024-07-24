4aac03fe2d5a038eb63db5f975008fd6abda3f36
bykezz <chibuikepascal31@gmail.com>
2024-07-24 13:14:11 +0100

    Handling errors

--- index.js
+++ index.js
@@ -7,24 +7,19 @@ const config = require("config");
 const Joi = require("joi");
 Joi.objectId = require("joi-objectid")(Joi);
 
-const users = require("./routes/users");
-const genres = require("./routes/genres");
-const customers = require("./routes/customers");
-const movies = require("./routes/movies");
-const rentals = require("./routes/rentals");
-const auth = require("./routes/auth");
-const error = require("./middleware/error");
-
 const app = express();
+require("./startup/routes")(app);
 
 process.on("uncaughtException", (ex) => {
   console.log("WE GOT AN UNCAUGHT EXECEPTION");
   winston.error(ex.message, ex);
+  process.exit(1);
 });
-
-process.on("uncaughtException", (ex) => {
-  console.log("WE GOT AN UNHANDLED REJECTION");
-  winston.error(ex.message, ex);
+winston.exceptions.handle(
+  new winston.transports.File({ filename: "uncaughtExceptions.log" })
+);
+process.on("unhandledRejection", (ex) => {
+  throw ex;
 });
 
 winston.add(new winston.transports.File({ filename: "logfile.log" }));
@@ -35,9 +30,6 @@ winston.add(
   })
 );
 
-const p = Promise.reject(new Error("failed again."));
-p.then(() => console.log("Done"));
-
 if (!config.get("jwtPrivateKey")) {
   console.error("FATAL ERROR: jwtPrivateKey is not defined");
   process.exit(1);
@@ -47,17 +39,6 @@ mongoose
   .connect("mongodb://localhost/vividly")
   .then(() => console.log("Connected to MongoDB..."))
   .catch((err) => console.error("Could not connect to MongoDB..."));
-
-app.use(express.json());
-
-app.use("/api/users", users);
-app.use("/api/genres", genres);
-app.use("/api/customers", customers);
-app.use("/api/movies", movies);
-app.use("/api/rentals", rentals);
-app.use("/api/auth", auth);
-
-app.use(error);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}....`));
