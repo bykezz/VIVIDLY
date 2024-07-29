009e5a442d82de1af013d00dca2408654116e1c5
bykezz <chibuikepascal31@gmail.com>
2024-07-29 17:06:52 +0100

    Today

--- tests/unit/models/user.test.js
+++ tests/unit/models/user.test.js
@@ -1,5 +1,11 @@
 const { User } = require("../../../models/user");
+const jwt = require("jsonwebtoken");
+const config = require("config");
 
 describe("user.generateAuthToken", () => {
-  items("should return a valid JWT ", () => {});
+  items("should return a valid JWT ", () => {
+    const user = new User({ _id: 1, isAdmin: true });
+    const token = user.generateAuthToken();
+    const decoded = jwt.verify(token, config.get("jwtPrivateKey"));
+  });
 });
