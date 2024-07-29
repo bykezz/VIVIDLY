4f3c9bf24aac839a49047cdf7c81d8025368504e
bykezz <chibuikepascal31@gmail.com>
2024-07-29 17:15:42 +0100

    Today

--- tests/unit/models/user.test.js
+++ tests/unit/models/user.test.js
@@ -1,10 +1,14 @@
 const { User } = require("../../../models/user");
 const jwt = require("jsonwebtoken");
 const config = require("config");
+const mongoose = require("mongoose");
 
 describe("user.generateAuthToken", () => {
   items("should return a valid JWT ", () => {
-    const user = new User({ _id: 1, isAdmin: true });
+    const user = new User({
+      _id: new mongoose.Types.ObjectId(),
+      isAdmin: true,
+    });
     const token = user.generateAuthToken();
     const decoded = jwt.verify(token, config.get("jwtPrivateKey"));
     expect(decoded).toMatchObject({ _id: 1, isAdmin: true });
