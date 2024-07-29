720171cd93aeea49e46d3515789daaa93a2f0c75
bykezz <chibuikepascal31@gmail.com>
2024-07-29 17:08:06 +0100

    Today

--- tests/unit/models/user.test.js
+++ tests/unit/models/user.test.js
@@ -7,5 +7,6 @@ describe("user.generateAuthToken", () => {
     const user = new User({ _id: 1, isAdmin: true });
     const token = user.generateAuthToken();
     const decoded = jwt.verify(token, config.get("jwtPrivateKey"));
+    expect(decoded).toMatchObject({ _id: 1, isAdmin: true });
   });
 });
