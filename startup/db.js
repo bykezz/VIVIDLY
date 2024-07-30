c89a59d2a9b93ef77435bdcda6a8072f62c533ec
bykezz <chibuikepascal31@gmail.com>
2024-07-30 09:07:18 +0100

    Today

--- startup/db.js
+++ startup/db.js
@@ -1,8 +1,9 @@
 const winston = require("winston");
 const mongoose = require("mongoose");
+const config = require("config");
 
 module.exports = function () {
   mongoose
-    .connect("mongodb://localhost/vividly")
+    .connect(config.get("db"))
     .then(() => console.log("Connected to MongoDB..."));
 };
